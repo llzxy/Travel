1ad01446aaa7e2cf80287394523061ef64788847
llzxy <lilei_hust@163.com>
2019-03-27 23:48:13 +0800

    新增轮播图 引入插件vue-awesome-swiper插件

--- src/main.js
+++ src/main.js
@@ -5,19 +5,26 @@ import App from './App'
 // import VueRouter from 'vue-router'
 import router from './router'
 import fastClick from 'fastclick'
+import ElementUI from 'element-ui'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
 fastClick.attach(document.body)
+Vue.use(ElementUI)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key=item.id>
+      <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div> -->
+    <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+  </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: '//img1.qunarzz.com/vc/b8/25/51/f6173dbd12c1a0f783abe73855.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: '//img1.qunarzz.com/vc/bf/9d/a3/67b7b37511fa26a78298bf1da1.jpg'
+        }
+
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
